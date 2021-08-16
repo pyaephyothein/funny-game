bd6688233a0caa560578c2bdee261d8749c9fa71
pyaephyothein <internetking.44@gmail.com>
2021-08-16 15:48:03 +0630

    Funny Game

---
+++ FunnyGameWithifelifelsefunction.py
@@ -0,0 +1,51 @@
+print("Funny Game")
+print("1.MyoTheingiKhin")
+print("2.KaungMyatHtay")
+print("3.Thutazaw")
+user_input = str(input("Enter Your Number :"))
+
+if user_input == "1":
+    print("1.A Pyo Gyi")
+    print("2.Gyout Thee")
+    print("3.I Love You")
+    user_input = str(input("Enter Your Number :"))
+    if user_input == "1":
+        print("A Pyo Gyi")
+    elif user_input == "2":
+        print("Gout Thee")
+    elif user_input == "3":
+        print("I Love You")
+    else:
+        print("invalid Choice")
+if user_input == "2":
+    print("1.Sparm Of Thuta")
+    print("2.Gay Soe Lay")
+    print("3.Pa Ka Myee Taw")
+    user_input = str(input("Enter Your Number :"))
+    if user_input == "1":
+        print("Sparm of Thuta")
+    elif user_input == "2":
+        print("Gay Soe Lay")
+    elif user_input == "3":
+        print("Pa Ka Myee Taw")
+    else:
+        print("invalid Choice")
+if user_input == "3":
+    print("1.Myanmar Porn Star")
+    print("2.Myanmarnese Bossi")
+    print("3.Another Level Kinh")
+    user_input = str(input("Enter Your Number :"))
+    if user_input == "1":
+        print("Myanmar Porn Star")
+    elif user_input == "2":
+        print("Myanmarnese Bossi")
+    elif user_input == "3":
+        print("Another Level King")
+    else:
+        print("invalid Choice")
+
+
+
+
+
+
